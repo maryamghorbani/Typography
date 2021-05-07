f7aa6fe255728bb9128d36aba10cd08a3231629d
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-07 16:05:35 +0430

    add text and text eventlistener

---
+++ script.js
@@ -0,0 +1,7 @@
+const hero = document.querySelector('.hero');
+hero.querySelector('h1');
+const walk = 500; //500px
+
+hero.addEventListener('mousemove', function (event) {
+    console.log(event);
+})
