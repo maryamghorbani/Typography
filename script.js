7365a6a8a58d36d8a418379ad3ad5d669c4f9680
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-07 16:40:29 +0430

    add text shadow and complete typograpgy

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const hero = document.querySelector('.hero');
-hero.querySelector('h1');
+const text = hero.querySelector('h1');
 const walk = 500; //500px
 
 hero.addEventListener('mousemove', function (event) {
-    console.log(event);
+    const { offsetWidth: width , offsetHeight: height } = hero;
+    let { offsetX: x, offsetY: y} = event;
+
+
+
+    x += event.target.offsetLeft;
+    y += event.target.offsetTop;
+
+    const xWalk = Math.round((x / width * walk) - (walk/2));
+    const yWalk = Math.round((y / height * walk) - (walk/2));
+
+    text.style.textShadow = `
+        ${xWalk}px ${yWalk}px 0 rgba(238, 82, 83, .7),
+        ${xWalk * -1}px ${yWalk}px 0 rgba(52, 31, 151, .7),
+        ${yWalk}px ${xWalk * -1}px 0 rgba(243, 104, 224, .7),
+        ${yWalk * -1}px ${xWalk}px 0 rgba(254, 202, 87, .7)
+    `
 })
